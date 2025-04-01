adf292ca3904c9e9478c7c56b7b41278b3b702d0
OrlandoRNBR <orlandoramosneto@gmail.com>
2025-03-31 21:20:49 -0300

    primeiro commit

---
+++ aula1-_arquivo-0/src/script.js
@@ -0,0 +1,20 @@
+function converter1(){
+  
+  valordiamante= prompt ("Digite um valor")
+esmeralda = 0.5
+alert (valordiamante * esmeralda + " esmeraldas")
+}
+
+function converter2(){
+  valorreal= prompt ("Digite um valor")
+dolar = 5.70
+alert ("R$" + valorreal * dolar)
+  
+}
+
+function converter3(){
+  valorreal= prompt ("Digite um valor")
+euro = 6.16
+alert ("R$" + valorreal * euro)
+  
+}
